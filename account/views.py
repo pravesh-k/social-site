8bb2de241370956a9a256221061a1b4048d3ce88
Pravesh <praveshnayak@gmail.com>
2021-11-09 01:02:46 +0530

    feature: view for user authetication and login

--- account/views.py
+++ account/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
-# Create your views here.
+
+# view for basic user login
+def user_login(request):
+    if request.mwthod == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                                request,
+                                username=cd['username'],
+                                password=cd['password']
+                                )
+            if user:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+        
+    else:
+        form = LoginForm()
+
+    args = {'form': form}
+    return render(request, 'account/login.html', args)
