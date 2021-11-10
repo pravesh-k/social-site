2efecddb10488d5f0b68ac394189767aff03e3e2
Pravesh <praveshnayak@gmail.com>
2021-11-11 01:11:02 +0530

    minor: descriptive comments

--- account/urls.py
+++ account/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     # path('login/', views.user_login, name='login'),     # previous login view w/o using Django auth views
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),       #LoginView refers to the html files located 
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),    #inside templates/registration by default
     ]
