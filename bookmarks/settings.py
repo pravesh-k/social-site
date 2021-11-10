96b1c2bb153483d19ba84cfcf9ad99b2a61a8aec
Pravesh <praveshnayak@gmail.com>
2021-11-11 01:30:49 +0530

    feature: setting to handle login and logout

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Account login and redirection settings
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
