ad13f5ceaae9cdfaa53c0ca5a88433d43751eee3
Pravesh <praveshnayak@gmail.com>
2021-11-11 01:29:25 +0530

    feature: path for dashboard

--- account/urls.py
+++ account/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),     # previous login view w/o using Django auth views
     path('login/', auth_views.LoginView.as_view(), name='login'),       #LoginView refers to the html files located 
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),    #inside templates/registration by default
+    path('', views.dashboard, name='dashboard'),
     ]
