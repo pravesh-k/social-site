acfde16dd1ac329934cdcd076da089a95cfa64cc
Pravesh <praveshnayak@gmail.com>
2021-11-13 00:01:15 +0530

    minor: descriptive comments

--- account/urls.py
+++ account/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),     # previous login view w/o using Django auth views
     path('login/', auth_views.LoginView.as_view(), name='login'),       #LoginView refers to the html files located 
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),    #inside templates/registration by default
-    path('', views.dashboard, name='dashboard'),
-    ]
+    path('', views.dashboard, name='dashboard'),                        #path empty indicates this is landing page url
+    ]                                                                   #its url will be [ip:port/account]
