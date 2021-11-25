52abc41c595a5f557cab1f6a764f1b0051d9dc2f
Pravesh <praveshnayak@gmail.com>
2021-11-25 23:27:49 +0530

    feature: password change paths

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),       #LoginView refers to the html files located 
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),    #inside templates/registration by default
     path('', views.dashboard, name='dashboard'),                        #path empty indicates this is landing page url
-    ]                                                                   #its url will be [ip:port/account]
+                                                                       #its url will be [ip:port/account]
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    ]
