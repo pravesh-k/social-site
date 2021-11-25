46c699b57bbc20af673695f5256ff1fc5a01290d
Pravesh <praveshnayak@gmail.com>
2021-11-26 00:18:13 +0530

    feature: Email setting, display mail on terminal

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+#Email Setting
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
