9a58b74135ac70219c92b62583d268fdbe8a5b89
Pravesh <praveshnayak@gmail.com>
2021-11-27 23:45:01 +0530

    feature: user registration form

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,24 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.forms import fields
 
 # Form to authenticate users against the database
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)      # the PasswordInput widget renders the 
                                                                 # password HTML element.
+
+# Form to create user registration
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+    
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('\nPasswords do not match.')
+        return cd['password2']
