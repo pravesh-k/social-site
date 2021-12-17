7848c6338dad056c0a0b0464c81201d3c68bf64c
Pravesh <praveshnayak@gmail.com>
2021-12-17 23:33:57 +0530

    minor: descriptive comments

--- account/urls.py
+++ account/urls.py
@@ -11,11 +11,13 @@ urlpatterns = [
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     
+    # URLs for password reset process
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     
+    # URL for registering a new user in the portal and then editing the profile details
     path('register/', views.register, name='register'),
     path('edit/', views.edit, name='edit'),
     ]
