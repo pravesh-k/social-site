0df479955d75106ad6d24674abfe5906d93f0066
Pravesh <praveshnayak@gmail.com>
2021-12-23 22:29:18 +0530

    minor: descriptive comments

--- account/forms.py
+++ account/forms.py
@@ -26,7 +26,7 @@ class UserRegistrationForm(forms.ModelForm):
 
 class UserEditForm(forms.ModelForm):
     class Meta:
-        model = User
+        model = User        # importing User form from the django contrib module
         fields = ('first_name', 'last_name', 'email')
 
 class ProfileEditForm(forms.ModelForm):
