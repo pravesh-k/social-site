18c211b95e0ec69276a3043139a15124402a95ac
Pravesh <praveshnayak@gmail.com>
2022-01-07 00:13:57 +0530

    feature: adding alert message on edit profile page

--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
 
@@ -98,6 +99,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated sucessfully')
+        else:
+            messages.error(request, 'Error updating your profile!')
     
     else:
         user_form = UserEditForm(instance=request.user)
